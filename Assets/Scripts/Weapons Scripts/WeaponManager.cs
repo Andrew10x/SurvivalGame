68a057709641bcec757bafa83157108b54fd7a35
Andrew <svt.andrey1631@gmail.com>
2022-02-28 18:35:23 +0200

    add possibility to change weapons

---
+++ Assets/Scripts/Weapons Scripts/WeaponManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    [SerializeField]
+    private WeaponHandler[] weapons;
+
+    private int current_Weapon_Index;
+    void Start()
+    {
+        current_Weapon_Index = 0;
+        weapons[current_Weapon_Index].gameObject.SetActive(true);
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            TurnOnSelectedWeapon(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            TurnOnSelectedWeapon(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            TurnOnSelectedWeapon(2);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            TurnOnSelectedWeapon(3);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            TurnOnSelectedWeapon(4);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            TurnOnSelectedWeapon(5);
+        }
+    }
+
+    void TurnOnSelectedWeapon(int weaponIndex)
+    {
+        if (weaponIndex == current_Weapon_Index)
+            return;
+
+        weapons[current_Weapon_Index].gameObject.SetActive(false);
+        weapons[weaponIndex].gameObject.SetActive(true);
+        current_Weapon_Index = weaponIndex;
+    }
+
+    public WeaponHandler GetCurrentSelectedWeapon()
+    {
+        return weapons[current_Weapon_Index];
+    }
+}
