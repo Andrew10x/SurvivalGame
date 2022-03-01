1cd5f8c65132de1d548afe37ffe699c427ee30e7
Andrew <svt.andrey1631@gmail.com>
2022-03-01 23:17:27 +0200

    cannibal chases the player

--- Assets/Scripts/Enemy Scripts/EnemyController.cs
+++ Assets/Scripts/Enemy Scripts/EnemyController.cs
@@ -86,16 +86,76 @@ public class EnemyController : MonoBehaviour
         {
             enemy_Anim.Walk(false);
         }
+
+        if(Vector3.Distance(transform.position, target.position) <= chase_Distance)
+        {
+            enemy_Anim.Walk(false);
+
+            enemy_State = EnemyState.CHASE;
+        }
     }
 
     void Chase()
     {
+        nav_Agent.isStopped = false;
+        nav_Agent.speed = run_Speed;
+        nav_Agent.SetDestination(target.position);
+
+        if (nav_Agent.velocity.sqrMagnitude > 0)
+        {
+            enemy_Anim.Run(true);
+        }
+        else
+        {
+            enemy_Anim.Run(false);
+        }
+
+        if(Vector3.Distance(transform.position, target.position) <= attack_Distance)
+        {
+            enemy_Anim.Run(false);
+            enemy_Anim.Walk(false);
+            enemy_State = EnemyState.ATTACK;
+
+            if (chase_Distance != current_Chase_Distance)
+            {
+                chase_Distance = current_Chase_Distance;
+            }
+        }
+        else if(Vector3.Distance(transform.position, target.position) > chase_Distance)
+        {
+            enemy_Anim.Run(false);
+
+            enemy_State = EnemyState.PATROL;
+
+            patrol_Timer = patrol_For_This_Time;
+            if (chase_Distance != current_Chase_Distance)
+            {
+                chase_Distance = current_Chase_Distance;
+            }
+        }
+
+
 
     }
 
     void Attack()
     {
+        nav_Agent.velocity = Vector3.zero;
+        nav_Agent.isStopped = true;
 
+        attack_Timer += Time.deltaTime;
+
+        if(attack_Timer > wait_Before_Attack)
+        {
+            enemy_Anim.Attack();
+            attack_Timer = 0f;
+        }
+
+        if(Vector3.Distance(transform.position, target.position) >
+            attack_Distance + chase_After_Attack_Distance)
+        {
+            enemy_State = EnemyState.CHASE;
+        }
     }
 
     void SetNewRandomDestination()
